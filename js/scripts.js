1d9cd6f61d35957c40e6f35489faf65602a19b73
JP_Rickabaugh <jprickabaugh@gmail.com>
2018-12-03 21:28:21 -0500

    have access to all page IDs with wiki API, albeit it is a bit brutish. Will look for more elegant solution if time allows

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
   console.log ('Scripts Loaded');
-  var apiParams1 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Dreamville_Records', exintro: 1};
+  var apiParams0 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Dreamville_Records', exintro: 1}; //pageid = 41775413
+  var apiParams1 = {action: 'query', prop: 'extracts', format: 'json', titles: 'J._Cole', exintro: 1}; //pageid = 23306612
+  var apiParams2 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Omen_(musician)', exintro: 1}; //pageid = 43576965
+  var apiParams3 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Bas_(rapper)', exintro: 1}; //pageid = 42614201
+  var apiParams4 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Cozz', exintro: 1}; //pageid = 43576183
+  var apiParams5 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Lute_(rapper)', exintro: 1}; //pageid = 48760021
+  var apiParams6 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Ari_Lennox', exintro: 1}; //pageid = 48760011
+  var apiParams7 = {action: 'query', prop: 'extracts', format: 'json', titles: 'J.I.D', exintro: 1}; //pageid = 53256071
+  var apiParams8 = {action: 'query', prop: 'extracts', format: 'json', titles: 'EarthGang', exintro: 1}; //pageid = 44017272
 
   $.ajax({ //as of now, it looks like a may have to do a separate ajax call for each description
-    "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams1),
+    "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams0),
     "dataType": "jsonp",
     //data: apiParams,
     success: function(response){
-        console.log(response.query.pages[41775413].extract);
-        var dreamvilleInfo = response.query.pages[41775413].extract;
-        $('#infodiv').append(dreamvilleInfo);
+  //      console.log(response.query.pages[41775413].extract);
+  //      var dreamvilleInfo = response.query.pages[41775413].extract;
+        console.log(response.query.pages)
+  //      $('#infodiv').append(dreamvilleInfo);
 
     }
   });
