ef351c6c2e326aee21f514876eef2a68286aeabb
JP_Rickabaugh <jprickabaugh@gmail.com>
2018-12-03 20:58:25 -0500

    figured out how to retrieve bio intros from wikipedia.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
   console.log ('Scripts Loaded');
-//  var myKey = config.MY_KEY;
-  var url = '';
-  var billboardData = [];
-  var query = '';
+  var apiParams1 = {action: 'query', prop: 'extracts', format: 'json', titles: 'Dreamville_Records', exintro: 1};
 
-  $('button').click(function(){
-    query = $('#query').val();
-    console.log(query);
-    var url = 'http://billboard.modulo.site/search/artist?q=' + query;
-    console.log(url);
+  $.ajax({ //as of now, it looks like a may have to do a separate ajax call for each description
+    "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams1),
+    "dataType": "jsonp",
+    //data: apiParams,
+    success: function(response){
+        console.log(response.query.pages[41775413].extract);
+        var dreamvilleInfo = response.query.pages[41775413].extract;
+        $('#infodiv').append(dreamvilleInfo);
 
-    $.ajax({
-      type:'GET',
-      dataType:'json',
-      data: billboardData,
-      url: url,
-      success:function(billboardData){
-        console.log(billboardData);
-        html = '';
-      }
-    });
-
-
-
+    }
   });
+/*    $.ajax({
+      type:'GET',
+      dataType:'jsonp',
+      data: data,
+      url: 'http://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&origin=*&page=pizza',
+      success:function(data){
+        console.log(data);
+      },
+      error:function(msg){
+        console.log('error');
+      }
+    }); */ //earlier test w parse method
 });
