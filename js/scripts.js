2469947047d2805915abbd7a6e65329effc60c3b
JP_Rickabaugh <jprickabaugh@gmail.com>
2018-12-04 11:44:04 -0500

    on click, artist wiki bio appears in box below

--- js/scripts.js
+++ js/scripts.js
@@ -18,44 +18,148 @@ $(document).ready(function(){
     success: function(response){
       console.log(response.query.pages[41775413].extract);
       var dreamvilleInfo = response.query.pages[41775413].extract;
-      $('#infodiv').append(dreamvilleInfo);
+      $('#defaultinfo').append(dreamvilleInfo);
     },
     error: function(msg){
       console.log('error');
     }
   });
 
-  $("#ai1").mouseover(function(){
-    $("#backgrounddiv").css("background-image", "url('img/KOD.jpg')");
-    $("#backgrounddiv").css("background-size", "100%");
-    $("#ah1").css("color", "black");
-  });
-  $("#ai1").mouseout(function(){
-    $("#backgrounddiv").css("background-image", "");
-    $("#ah1").css("color", "");
-  });
-
-  $("#ai2").mouseover(function(){
-    $("#backgrounddiv").css("background-image", "url('img/elephanteyes.jpg')");
-    $("#backgrounddiv").css("background-size", "100%");
-    $("#ah2").css("color", "black");
-  });
-  $("#ai2").mouseout(function(){
-    $("#backgrounddiv").css("background-image", "");
-    $("#ah2").css("color", "");
-  });
-
-  //use $(whatever).onhover or whatever for following bios
-/*    $.ajax({
-      type:'GET',
-      dataType:'jsonp',
-      data: data,
-      url: 'http://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&origin=*&page=pizza',
-      success:function(data){
-        console.log(data);
+  $('#ai1').click(function(){
+    $('#defaultinfo, #info2, #info3, #info4, #info5, #info6, #info7, #info8').css("display", "none");
+    $("#info1").empty();
+    $('#info1').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams1),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[23306612].extract);
+        var jColeInfo = response.query.pages[23306612].extract;
+        $('#info1').append(jColeInfo);
       },
-      error:function(msg){
+      error: function(msg){
         console.log('error');
       }
-    }); */ //earlier test w parse method
+    });
+  });
+  $('#ai2').click(function(){
+    $('#defaultinfo, #info1, #info3, #info4, #info5, #info6, #info7, #info8').css("display", "none");
+    $("#info2").empty();
+    $('#info2').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams2),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[43576965].extract);
+        var omenInfo = response.query.pages[43576965].extract;
+        $('#info2').append(omenInfo);
+      },
+      error: function(msg){
+        console.log('error');
+      }
+    });
+  });
+  $('#ai3').click(function(){
+    $('#defaultinfo, #info1, #info2, #info4, #info5, #info6, #info7, #info8').css("display", "none");
+    $("#info3").empty();
+    $('#info3').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams3),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[42614201].extract);
+        var basInfo = response.query.pages[42614201].extract;
+        $('#info3').append(basInfo);
+      },
+      error: function(msg){
+        console.log('error');
+      }
+    });
+  });
+  $('#ai4').click(function(){
+    $('#defaultinfo, #info1, #info2, #info3, #info5, #info6, #info7, #info8').css("display", "none");
+    $("#info4").empty();
+    $('#info4').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams4),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[43576183].extract);
+        var cozzInfo = response.query.pages[43576183].extract;
+        $('#info4').append(cozzInfo);
+      },
+      error: function(msg){
+        console.log('error');
+      }
+    });
+  });
+  $('#ai5').click(function(){
+    $('#defaultinfo, #info1, #info2, #info3, #info4, #info6, #info7, #info8').css("display", "none");
+    $("#info5").empty();
+    $('#info5').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams5),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[48760021].extract);
+        var luteInfo = response.query.pages[48760021].extract;
+        $('#info5').append(luteInfo);
+      },
+      error: function(msg){
+        console.log('error');
+      }
+    });
+  });
+  $('#ai6').click(function(){
+    $('#defaultinfo, #info1, #info2, #info3, #info4, #info5, #info7, #info8').css("display", "none");
+    $("#info6").empty();
+    $('#info6').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams6),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[48760011].extract);
+        var ariLennoxInfo = response.query.pages[48760011].extract;
+        $('#info6').append(ariLennoxInfo);
+      },
+      error: function(msg){
+        console.log('error');
+      }
+    });
+  });
+  $('#ai7').click(function(){
+    $('#defaultinfo, #info1, #info2, #info3, #info4, #info5, #info6, #info8').css("display", "none");
+    $("#info7").empty();
+    $('#info7').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams7),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[53256071].extract);
+        var jidInfo = response.query.pages[53256071].extract;
+        $('#info7').append(jidInfo);
+      },
+      error: function(msg){
+        console.log('error');
+      }
+    });
+  });
+  $('#ai8').click(function(){
+    $('#defaultinfo, #info1, #info2, #info3, #info4, #info5, #info6, #info7').css("display", "none");
+    $("#info8").empty();
+    $('#info8').css("display", "block");
+    $.ajax({
+      "url":"https://en.wikipedia.org/w/api.php?" + $.param(apiParams8),
+      "dataType": "jsonp",
+      success: function(response){
+        console.log(response.query.pages[44017272].extract);
+        var earthgangInfo = response.query.pages[44017272].extract;
+        $('#info8').append(earthgangInfo);
+      },
+      error: function(msg){
+        console.log('error');
+      }
+    });
+  });
+
 });
